eeab4276380e1c84bda736685d97e8bfb6921ef8
Fransuelton <elton6103@gmail.com>
2024-11-28 21:01:15 -0300

    ✨ feat: add new code

---
+++ variaveis.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    // Declara a variável idade com tipo inteiro
+    int idade;
+
+    // Atribui o valor 19 a variável idade
+    idade = 19;
+
+    // Usa a função printf para mostrar a mensagem e idade no console
+    printf("Minha idade é: %d\n", idade);
+
+    return 0;
+}
