bb9ecb811e55133e28ebd3a751a4f1adcf263af4
Fransuelton <elton6103@gmail.com>
2024-12-09 11:22:26 -0300

    ✨ feat: add printf program to display personal information

---
+++ printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int idade = 30;
+    float altura = 1.75;
+    char nome[] = "João";
+    printf("Olá, meu nome é %s e tenho %d anos. \
+nome, idade");
+    printf("Minha altura é %.2f metros.\n", altura);
+    return 0;
+}
